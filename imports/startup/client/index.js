824b46a9f5bf298f414970e78f79e11b27398a0d
t_jeanneau <hi@thomasjeanneau.com>
2017-02-07 14:33:00 +0100

    install ESLint

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,5 +2,5 @@
  * Created by thomasjeanneau on 07/02/2017.
  */
 
-import './useraccounts-configuration.js';
-import './routes.js';
+import './useraccounts-configuration';
+import './routes';
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -3,11 +3,11 @@
  */
 
 // This defines a starting set of data to be loaded if the app is loaded with an empty db.
-import '../imports/startup/server/fixtures.js';
+import './fixtures';
 
 // Set up some rate limiting and other important security settings.
-import '../imports/startup/server/security.js';
+import './security';
 
 // This defines all the collections, publications and methods that the application provides
 // as an API to the client.
-import '../imports/api/api.js';
+import '../../api/api';
